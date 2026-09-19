e68f43824039478e058145fbb18e4d269d533029
Jaspreet 5 <jaspreet2005g@gmail.com>
2026-09-19 18:04:47 +0530

    Rebuilt Core Technologies

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 function githubRedirect(){
     window.location.href = "https://github.com/Jaspreet-5"
 }
+
+(function renderCoreTechnologies() {
+
+    fetch("./pages/core-technologies.html")
+        .then(response => response.text())
+        .then(html => {
+            document.getElementById("core-technologies").innerHTML = html;
+        });
+
+})();
